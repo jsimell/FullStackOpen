44b482c81f7c12d539f982fa732dd41a4e9d3fe3
Jiri Simell <jiri.simell@gmail.com>
2024-05-31 15:40:48 +0300

    1.11* unicafe step 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,36 +1,73 @@
 import { useState } from "react"
 
+const StatisticLine = (props) => {
+  const {text, value} = props
+  return (
+    <>
+      <td>
+        {text}
+      </td>
+      <td>
+        {value} {text === "positive" ? "%": ""}
+      </td>
+    </>
+  )
+}
+
+const Statistics = (props) => {
+  const {good, neutral, bad, all} = props
+
+  if (all === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  } else {
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <StatisticLine text="good" value={good} />
+          </tr>
+          <tr>
+            <StatisticLine text="neutral" value={neutral} />
+          </tr>
+          <tr>
+            <StatisticLine text="bad" value={bad} />
+          </tr>
+          <tr>
+            <StatisticLine text="all" value={all} />
+          </tr>
+          <tr>
+            <StatisticLine text="average" value={all === 0 ? 0: (good - bad) / all} />
+          </tr>
+          <tr>
+            <StatisticLine text="positive" value={all === 0 ? 0: good / all} />
+          </tr>
+        </tbody>
+
+      </table>
+    )
+  }
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const handleReview = (type) => () => {
-    console.log(good)
-    if (type === "good") {
-      setGood(good + 1)
-    } else if (type === "neutral") {
-      setNeutral(neutral + 1)
-    } else {
-      setBad(bad + 1)
-    }
-  } 
+  const handleGood = () => () => setGood(good + 1)
+  const handleNeutral = () => () => setNeutral(neutral + 1)
+  const handleBad = () => () => setBad(bad + 1)
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={handleReview("good")}>good</button>
-      <button onClick={handleReview("neutral")}>neutral</button>
-      <button onClick={handleReview("bad")}>bad</button>
+      <button onClick={handleGood("good")}>good</button>
+      <button onClick={handleNeutral("neutral")}>neutral</button>
+      <button onClick={handleBad("bad")}>bad</button>
       <h1>statistics</h1>
-      <p>
-        good {good}
-        <br/>
-        neutral {neutral}
-        <br/>
-        bad {bad}
-      </p>
+      <Statistics good={good} neutral={neutral} bad={bad} all={good + neutral + bad} />
     </div>
   )
 }
